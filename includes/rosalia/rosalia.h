bc62d1469137b2e05be879f6b2a32748e37ddd73
Urs Kober <urs.kober@mailbox.tu-dresden.de>
2022-11-25 19:32:57 +0100

    import surena utils

--- includes/rosalia/rosalia.h
+++ includes/rosalia/rosalia.h
@@ -1,1 +1,9 @@
 #pragma once
+
+#include "rosalia/base64.h"
+#include "rosalia/noise.h"
+#include "rosalia/rand.h"
+#include "rosalia/raw_stream.h"
+#include "rosalia/semver.h"
+#include "rosalia/serialization.h"
+#include "rosalia/timestamp.h"
---
+++ src/impl/semver.c
@@ -0,0 +1,22 @@
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "rosalia/semver.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+bool SEMVER_equal(semver l, semver r)
+{
+    return l.major == r.major && l.minor == r.minor && l.patch == r.patch;
+}
+
+bool SEMVER_satisfies(semver required, semver provided)
+{
+    return provided.major == required.major && (provided.minor > required.minor || (provided.minor == required.minor && provided.patch >= required.patch));
+}
+
+#ifdef __cplusplus
+}
+#endif
