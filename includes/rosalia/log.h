a0aa07ebbc31ca019d41254f64f31165e4762868
Urs Kober <urs.kober@mailbox.tu-dresden.de>
2023-08-25 17:01:44 +0200

    add multiple log outputs

--- includes/rosalia/log.h
+++ includes/rosalia/log.h
@@ -1,24 +1,22 @@
 #pragma once
 
+//TODO make this single header
+
+#include <assert.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 /*TODO api? whats the intended usage/pattern here?
-- need opt-in thread safety across single logger
 - backends for writing to files, printing, in-memory cycle buffer, //TODO composable??
 - debug log statements that compile out of release builds, but can be on demand disabled
-
-
-usage examples:
-logger* log;
-LOG_CREATE(&log);
-LOG_DESTROY(&log);
-LOGF(log, LOGL_INFO, "thing");
-LOGF_FATAL(log, "thing")
-
 */
 
 typedef enum LOGL_E {
@@ -33,6 +31,73 @@ typedef enum LOGL_E {
     LOGL_COUNT,
     LOGL_SIZE_MAX = UINT8_MAX,
 } LOGL;
+
+extern const char* logl_names[LOGL_COUNT];
+
+typedef struct rosa_logger_output_s rosa_logger_output;
+
+// this callback will be called by a logger everytime a new log line comes up
+// str end is the first character NOT part of the string, i.e. the null terminator
+typedef void rosa_logger_output_push_line_t(rosa_logger_output* output, const char* str, const char* str_end);
+
+typedef void rosa_logger_output_destroy_t(rosa_logger_output* output);
+
+struct rosa_logger_output_s {
+    uint32_t id;
+    void* private_data;
+    rosa_logger_output_push_line_t* push_line;
+    rosa_logger_output_destroy_t* destroy;
+};
+
+typedef struct rosa_logger_s {
+    char* log_name;
+    char* pre_layout;
+    char* post_layout;
+    bool concurrent_usage;
+    //TODO child loggers?
+    uint32_t next_output_id;
+    rosa_logger_output* outputs; // rosa_vec<rosa_logger_output>
+} rosa_logger;
+
+void rosa_log_create(rosa_logger** log, const char* log_name, bool concurrent_usage, const char* pre_layout, const char* post_layout);
+
+void rosa_log_destroy(rosa_logger** log);
+
+uint32_t rosa_log_output_add(rosa_logger* log, rosa_logger_output output);
+
+bool rosa_log_output_remove(rosa_logger* log, uint32_t output_id, rosa_logger_output* output);
+
+size_t rosa__log_internal_layout(char* buf, size_t len, rosa_logger* log, const char* layout);
+
+void rosa__log_internal_log(rosa_logger* log, LOGL lvl, const char* str, const char* str_end);
+
+void rosa__log_internal_logf(rosa_logger* log, LOGL lvl, const char* fmt, ...);
+
+void rosa__log_internal_logfv(rosa_logger* log, LOGL lvl, const char* fmt, va_list args);
+
+//TODO these all should pass their file and line number to the log function
+
+#define ROSA_LOG(log, lvl, str, str_end) (rosa__log_internal_log(log, lvl, str, str_end))
+#define ROSA_LOGF(log, lvl, fmt, ...) (rosa__log_internal_logf(log, lvl, fmt, ##__VA_ARGS__))
+#define ROSA_LOGFV(log, lvl, fmt, args) (rosa__log_internal_logfv(log, lvl, fmt, args))
+
+#define ROSA_LOG_DEBUG(log, str, str_end) (rosa__log_internal_log(log, LOGL_DEBUG, str, str_end))
+#define ROSA_LOG_INFO(log, str, str_end) (rosa__log_internal_log(log, LOGL_INFO, str, str_end))
+#define ROSA_LOG_WARN(log, str, str_end) (rosa__log_internal_log(log, LOGL_WARN, str, str_end))
+#define ROSA_LOG_ERROR(log, str, str_end) (rosa__log_internal_log(log, LOGL_ERROR, str, str_end))
+#define ROSA_LOG_FATAL(log, str, str_end) (rosa__log_internal_log(log, LOGL_FATAL, str, str_end))
+
+#define ROSA_LOGF_DEBUG(log, fmt, ...) (rosa__log_internal_logf(log, LOGL_DEBUG, fmt, ##__VA_ARGS__))
+#define ROSA_LOGF_INFO(log, fmt, ...) (rosa__log_internal_logf(log, LOGL_INFO, fmt, ##__VA_ARGS__))
+#define ROSA_LOGF_WARN(log, fmt, ...) (rosa__log_internal_logf(log, LOGL_WARN, fmt, ##__VA_ARGS__))
+#define ROSA_LOGF_ERROR(log, fmt, ...) (rosa__log_internal_logf(log, LOGL_ERROR, fmt, ##__VA_ARGS__))
+#define ROSA_LOGF_FATAL(log, fmt, ...) (rosa__log_internal_logf(log, LOGL_FATAL, fmt, ##__VA_ARGS__))
+
+#define ROSA_LOGFV_DEBUG(log, fmt, args) (rosa__log_internal_logfv(log, LOGL_DEBUG, fmt, args))
+#define ROSA_LOGFV_INFO(log, fmt, args) (rosa__log_internal_logfv(log, LOGL_INFO, fmt, args))
+#define ROSA_LOGFV_WARN(log, fmt, args) (rosa__log_internal_logfv(log, LOGL_WARN, fmt, args))
+#define ROSA_LOGFV_ERROR(log, fmt, args) (rosa__log_internal_logfv(log, LOGL_ERROR, fmt, args))
+#define ROSA_LOGFV_FATAL(log, fmt, args) (rosa__log_internal_logfv(log, LOGL_FATAL, fmt, args))
 
 #ifdef __cplusplus
 }
--- src/tests/log.c
+++ src/tests/log.c
@@ -8,10 +8,13 @@
 
 #include <assert.h>
 #include <stdarg.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 
-const char* loga[LOGL_COUNT] = {
+#include <rosalia/vector.h>
+
+const char* logl_names[LOGL_COUNT] = {
     [LOGL_NONE] = "NONE",
     [LOGL_DEBUG] = "DEBUG",
     [LOGL_INFO] = "INFO",
@@ -20,62 +23,107 @@ const char* loga[LOGL_COUNT] = {
     [LOGL_FATAL] = "FATAL",
 };
 
-typedef struct rosa_logger_s {
-    const char* log_name;
-    const char* pre_layout;
-    const char* post_layout;
-    //TODO callback and child loggers?
-} rosa_logger;
-
-void rosa_log_create(rosa_logger** log, const char* log_name, const char* pre_layout, const char* post_layout);
-void rosa_log_destroy(rosa_logger** log);
-void rosa_log(rosa_logger* log, LOGL lvl, const char* str, const char* str_end);
-void rosa_logf(rosa_logger* log, LOGL lvl, const char* fmt, ...);
-void rosa_logfv(rosa_logger* log, LOGL lvl, const char* fmt, va_list args);
-
-void rosa_log_create(rosa_logger** log, const char* log_name, const char* pre_layout, const char* post_layout)
+void rosa_log_create(
+    rosa_logger** log,
+    const char* log_name,
+    bool concurrent_usage, //TODO this / using the mutexes should be guarded by a define, i.e. make it possible to use logging without providing pthreads
+    const char* pre_layout,
+    const char* post_layout
+)
 {
     *log = malloc(sizeof(rosa_logger));
     rosa_logger* logi = *log;
     logi->log_name = strdup(log_name);
     logi->pre_layout = strdup(pre_layout);
     logi->post_layout = strdup(post_layout);
+    logi->concurrent_usage = concurrent_usage;
+    logi->next_output_id = 0;
+    VEC_CREATE(&logi->outputs, 1);
 }
 
 void rosa_log_destroy(rosa_logger** log)
 {
     rosa_logger* logi = *log;
+    if (logi->concurrent_usage == true) {
+        //TODO locking..
+    }
     free(logi->log_name);
     free(logi->pre_layout);
     free(logi->post_layout);
     free(logi);
+    for (size_t i = 0; i < VEC_LEN(&logi->outputs); i++) {
+        logi->outputs[i].destroy(&logi->outputs[i]);
+    }
+    VEC_DESTROY(&logi->outputs);
     *log = NULL;
+}
+
+// ownership of the private data in output is given to the logger and will be destroyed through the callback later
+uint32_t rosa_log_output_add(rosa_logger* log, rosa_logger_output output)
+{
+    if (log->concurrent_usage == true) {
+        //TODO locking..
+    }
+    output.id = log->next_output_id++;
+    VEC_PUSH(&log->outputs, output);
+    if (log->concurrent_usage == true) {
+        //TODO locking..
+    }
+    return output.id;
+}
+
+// must use an id previously returned through output_add and not yet removed
+// returns true if the removal took place
+// NOTE: a removed logger is NOT destroyed, it is moved to output and the ownership of everything therein with it
+bool rosa_log_output_remove(rosa_logger* log, uint32_t output_id, rosa_logger_output* output)
+{
+    if (log->concurrent_usage == true) {
+        //TODO locking..
+    }
+    for (size_t i = 0; i < VEC_LEN(&log->outputs); i++) {
+        if (log->outputs[i].id == output_id) {
+            *output = log->outputs[i];
+            VEC_REMOVE_SWAP(&log->outputs, i);
+            if (log->concurrent_usage == true) {
+                //TODO locking..
+            }
+            return true;
+        }
+    }
+    if (log->concurrent_usage == true) {
+        //TODO locking..
+    }
+    return false;
 }
 
 // behaves similar to how snprintf would
 size_t rosa__log_internal_layout(char* buf, size_t len, rosa_logger* log, const char* layout)
 {
     //TODO
+    if (buf == NULL) {
+        return 100;
+    }
+    return sprintf(buf, "%s", layout);
 }
 
-void rosa_log(rosa_logger* log, LOGL lvl, const char* str, const char* str_end)
+void rosa__log_internal_log(rosa_logger* log, LOGL lvl, const char* str, const char* str_end)
 {
     if (str_end == NULL) {
-        return rosa_logf(log, lvl, "%s", str);
+        return rosa__log_internal_logf(log, lvl, "%s", str);
     } else {
-        return rosa_logf(log, lvl, "%.*s", str_end - str, str);
+        return rosa__log_internal_logf(log, lvl, "%.*s", str_end - str, str);
     }
 }
 
-void rosa_logf(rosa_logger* log, LOGL lvl, const char* fmt, ...)
+void rosa__log_internal_logf(rosa_logger* log, LOGL lvl, const char* fmt, ...)
 {
     va_list args;
     va_start(args, fmt);
-    rosa_logfv(log, lvl, fmt, args);
+    rosa__log_internal_logfv(log, lvl, fmt, args);
     va_end(args);
 }
 
-void rosa_logfv(rosa_logger* log, LOGL lvl, const char* fmt, va_list args)
+void rosa__log_internal_logfv(rosa_logger* log, LOGL lvl, const char* fmt, va_list args)
 {
     assert(log != NULL);
     assert(lvl > LOGL_NONE && lvl < LOGL_COUNT);
@@ -92,39 +140,44 @@ void rosa_logfv(rosa_logger* log, LOGL lvl, const char* fmt, va_list args)
     char* wbuf = pbuf;
     wbuf += rosa__log_internal_layout(wbuf, len_pre + 1, log, log->pre_layout);
     wbuf += vsnprintf(wbuf, len_user + 1, fmt, args);
-    wbuf += rosa__log_internal_layout(wbuf, len_post, log, log->post_layout);
+    wbuf += rosa__log_internal_layout(wbuf, len_post + 1, log, log->post_layout);
 
+    if (log->concurrent_usage == true) {
+        //TODO locking..
+    }
     // push to callbacks etc..
-    printf("%s", pbuf);
+    for (size_t i = 0; i < VEC_LEN(&log->outputs); i++) {
+        log->outputs[i].push_line(&log->outputs[i], pbuf, wbuf + 1);
+    }
+    if (log->concurrent_usage == true) {
+        //TODO locking..
+    }
+
     free(pbuf);
 }
 
-//TODO these all should pass their file and line number to the log function
+void logger_output_stdout_push_line(rosa_logger_output* output, const char* str, const char* str_end)
+{
+    printf("%s", str);
+}
 
-#define ROSA_LOG_DEBUG(log, str, str_end) (rosa_log(log, LOGL_DEBUG, str, str_end))
-#define ROSA_LOG_INFO(log, str, str_end) (rosa_log(log, LOGL_INFO, str, str_end))
-#define ROSA_LOG_WARN(log, str, str_end) (rosa_log(log, LOGL_WARN, str, str_end))
-#define ROSA_LOG_ERROR(log, str, str_end) (rosa_log(log, LOGL_ERROR, str, str_end))
-#define ROSA_LOG_FATAL(log, str, str_end) (rosa_log(log, LOGL_FATAL, str, str_end))
+void logger_output_stdout_destroy(rosa_logger_output* output)
+{
+    // pass
+}
 
-#define ROSA_LOGF_DEBUG(log, fmt, ...) (rosa_logf(log, LOGL_DEBUG, fmt, ##__VA_ARGS__))
-#define ROSA_LOGF_INFO(log, fmt, ...) (rosa_logf(log, LOGL_INFO, fmt, ##__VA_ARGS__))
-#define ROSA_LOGF_WARN(log, fmt, ...) (rosa_logf(log, LOGL_WARN, fmt, ##__VA_ARGS__))
-#define ROSA_LOGF_ERROR(log, fmt, ...) (rosa_logf(log, LOGL_ERROR, fmt, ##__VA_ARGS__))
-#define ROSA_LOGF_FATAL(log, fmt, ...) (rosa_logf(log, LOGL_FATAL, fmt, ##__VA_ARGS__))
-
-#define ROSA_LOGFV_DEBUG(log, fmt, args) (rosa_logf(log, LOGL_DEBUG, fmt, args))
-#define ROSA_LOGFV_INFO(log, fmt, args) (rosa_logf(log, LOGL_INFO, fmt, args))
-#define ROSA_LOGFV_WARN(log, fmt, args) (rosa_logf(log, LOGL_WARN, fmt, args))
-#define ROSA_LOGFV_ERROR(log, fmt, args) (rosa_logf(log, LOGL_ERROR, fmt, args))
-#define ROSA_LOGFV_FATAL(log, fmt, args) (rosa_logf(log, LOGL_FATAL, fmt, args))
+rosa_logger_output logger_output_stdout = (rosa_logger_output){
+    .push_line = logger_output_stdout_push_line,
+    .destroy = logger_output_stdout_destroy,
+};
 
 void run_test_log()
 {
     rosa_logger* log;
-    rosa_log_create(&log, "test logger", "%d{%Y-%m-%d_%H:%M:%S} %l [%p]: ", "\n");
+    rosa_log_create(&log, "test logger", false, "%d{%Y-%m-%d %H:%M:%S}.%f %l [%p]: ", "\n");
+    rosa_log_output_add(log, logger_output_stdout);
 
-    rosa_logf(log, LOGL_INFO, "1");
+    ROSA_LOGF(log, LOGL_INFO, "1");
     ROSA_LOGF_ERROR(log, "abc");
 
     rosa_log_destroy(&log);
